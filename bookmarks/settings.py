9ff15aa421f03c286bbf6e02eef32f81e0d66be3
Stanislav Gusev <st.al.gusev@gmail.com>
2023-09-21 15:08:24 +0300

    Added https access, installed necessary packages

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-rwmjy9_wg61hf-&e!pgo=d2^a^l(0lqmhb1k8+6=pkueuq2=sv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "social_django",
+    'django_extensions',
 ]
 
 MIDDLEWARE = [      # Промежуточные программные компоненты
