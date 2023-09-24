794c69bfec1b460af56df63aeb6f1f6c101da7cb
Stanislav Gusev <st.al.gusev@gmail.com>
2023-09-24 23:52:56 +0300

    Developed Image model, form for adding a bookmark

--- images/views.py
+++ images/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import ImageCreateForm
 
-# Create your views here.
+
+@login_required
+def image_create(request):
+    if request.method == 'POST':
+        # form is sent
+        form = ImageCreateForm(data=request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            new_image = form.save(commit=False)
+            # assign the current user to an element
+            new_image.user = request.user
+            new_image.save()
+            messages.success(request,
+                             'Image added successfully')
+            # redirect to a view of detailed information
+            # about the newly created element
+            return redirect(new_image.get_absolute_url())
+    else:
+        # скомпоновать форму с данными, предоставленными
+        # букмарклетом методом GET
+        form = ImageCreateForm(data=request.GET)
+    return render(request,
+                  'images/image/create.html',
+                  {'section': 'images',
+                   'form': form})
