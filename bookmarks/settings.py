695f81d497f9dd8fe0e6e62f7b2346985ccc8369
stnaus <82822832+stnaus@users.noreply.github.com>
2023-11-16 01:17:46 +0300

    Update settings.py

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -157,11 +157,11 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 
-SOCIAL_AUTH_TWITTER_KEY = 'p0PZfO8aPZ67fkqBycC8WzqRz' # API Key
-SOCIAL_AUTH_TWITTER_SECRET = 'tktFdteXPGrpkp7djAAz90Nhsn1sb6PO6jW2Q1AzlfK3YXDd4X' # API Key Secret
+SOCIAL_AUTH_TWITTER_KEY = 'add-your-api-key' # API Key
+SOCIAL_AUTH_TWITTER_SECRET = 'add-your-api-key' # API Key Secret
 
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '643976947666-eapn8kfp0iugirimr5ih97lp8q1dvtb7.apps.googleusercontent.com' # ИД клиента Google
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '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' # Секрет клиента Google
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = 'add-your-id' # ИД клиента Google
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'add-your-key' # Секрет клиента Google
 
 
 SOCIAL_AUTH_PIPELINE = [
